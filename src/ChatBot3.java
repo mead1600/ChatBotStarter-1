a87ba55693532801bbb13feb35a51467c2375531
nana4013 <mgyawu0974@bths.edu>
2018-10-19 00:52:55 -0400

    Weeb monkey shtuff

--- src/ChatBot3.java
+++ src/ChatBot3.java
@@ -30,48 +30,166 @@ public class ChatBot3
 
 			statement = in.nextLine();
 			//getResponse handles the user reply
-			System.out.println(teamQuestions(statement));
+			if(statement.toLowerCase().equals("switch"))
+			{
+				System.out.println("Which chatbot would you like to switch to? Handball, Football, or Basketball?");
+				break;
+			}
+			System.out.println(getResponse(statement));
 
 
 		}
 
 	}
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
 	 * @return a greeting
-	 */	
+	 */
 	public String getGreeting()
 	{
-		return "What is your favorite football team?";
+		return "Do you like football?";
 	}
 
-	public String teamQuestions(String statement)
+	/**
+	 * Gives a response to a user statement
+	 *
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
 	{
 		String response = "";
+
 		if (statement.length() == 0)
 		{
 			response = "Say something, please.";
-			return response;
 		}
-		else if (statement.equals("I don't have a favorite team"))
-		{
-			response = transformIDontToStatement(statement);
-			return response;
-		}
-		else if (statement.equals("I dont have a favorite team" ))
-		{
-			String tempWord = statement.substring(0,5) + "'" + statement.substring(5);
-			response = transformIDontToStatement(tempWord);
-			return response;
-		}
-		else{
-			response = teamResponse(statement);
-			System.out.println(response);
-			return finalEnthusiasticResponse(response);
-			}
 
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why did you choose football then?";
+			emotion--;
 		}
-	private String transformIDontToStatement(String statement)
+		else if (findKeyword(statement, "nope") >= 0)
+		{
+			response = "Why did you choose football then?";
+			emotion--;
+		}
+
+		else if (findKeyword(statement, "nah") >= 0)
+		{
+			response = "Why did you choose football then?";
+			emotion--;
+		}
+
+		else if (findKeyword(statement, "naw") >= 0)
+		{
+			response = "Why did you choose football then?";
+			emotion--;
+		}
+
+		else if (findKeyword(statement, "you suck") >= 0)
+		{
+			response = "Why are you so mean to me?";
+			emotion--;
+		}
+		else if (findKeyword(statement, "yes") >= 0)
+		{
+			emotion++;
+			response = "really? Awesome! "+getRandomResponse();
+		}
+
+		else if (findKeyword(statement, "yep") >= 0)
+		{
+			response = "really? Awesome! "+getRandomQuestion();
+			emotion++;
+		}
+		else if (findKeyword(statement, "yeah") >= 0)
+		{
+			response = "really? Awesome! "+getRandomQuestion();
+			emotion++;
+		}
+		else if (findKeyword(statement, "yea") >= 0)
+		{
+			emotion++;
+			response = "really? Awesome! "+getRandomResponse();
+		}
+		else if(findKeyword(statement, "I don't know") >= 0)
+		{
+			response = "c'mon man you have to know";
+		}
+		else if(findKeyword(statement, "I dont know") >= 0)
+		{
+			response = "c'mon man you have to know";
+		}
+		else if(findKeyword(statement, "I do not know") >= 0)
+		{
+			response = "c'mon man you have to know";
+		}
+		else if (findKeyword(statement, "I love to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+		else if (findKeyword(statement, "you", 0) >= 0)
+		{
+			response = transformIYouStatement(statement);
+		}
+		else if (findKeyword(statement, "I",0) >= 0)
+		{
+			if (findKeyword(statement, "you") >= 0)
+			{
+				response = 	transformIYouStatement(statement);
+			}
+			else {
+				response = transformIWantStatement(statement);
+			}
+		}
+		else if (findKeyword(statement,"because i am") >= 0)
+		{
+			response = transformBecauseIAm(statement);
+		}
+		else if (findKeyword(statement,"because it") >= 0)
+		{
+			response = transformBecauseIt(statement);
+		}
+		else if (findKeyword(statement, "because i") >= 0)
+		{
+			response = transformBecauseI(statement);
+		}
+		else if (findKeyword(statement,"because you are") >= 0)
+		{
+			response = transformBecauseYouAre(statement);
+		}
+		else if (findKeyword(statement,"because you") >= 0)
+		{
+			response = transformBecauseYou(statement);
+		}
+		else if (findKeyword(statement,"because") >= 0)
+		{
+			response = "why?";
+		}
+		else if (findKeyword(statement,"cuz you are") >= 0)
+		{
+			response = transformCuzYouAre(statement);
+		}
+		else if (findKeyword(statement,"cuz you") >= 0)
+		{
+			response = transformCuzYou(statement);
+		}
+		else if (findKeyword(statement,"cuz") >= 0)
+		{
+			response = "why?";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+
+		return response;
+	}
+
+	private String transformBecauseIt(String statement)
 	{
 		statement = statement.trim();
 		String lastChar = statement.substring(statement
@@ -81,85 +199,14 @@ public class ChatBot3
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		return "Why don't you have a favorite team?";
+		int psn = findKeyword (statement, "because it", 0);
+		String restOfStatement = statement.substring(psn + 10).trim();
+		return "Why is it " + restOfStatement + "?";
 	}
 
-	private String teamResponse(String statement)
+	private String transformBecauseI(String statement)
 	{
-		double questionNum = Math.random();
-		String response = "";
-		if(questionNum >= 0.5)
-		{
-			return "Why is " + statement + " your favorite team?";
-		}
-		else
-		{
-			return "Would you like to go with me to a " + statement + "'s game?";
-		}
-	}
-
-	private String finalEnthusiasticResponse(String response)
-	{
-		Scanner reason = new  Scanner(System.in);
-		String reasonOut = reason.nextLine();
-		if(response.substring(0,6).equals("Why is"))
-		{
-			return "Man, I'm glad that you have a team that you can love that much";
-		}
-		else if(response.substring(0,6).equals("Would "))
-		{
-			if(reasonOut.toLowerCase().equals("no"))
-			{
-				return "Oh ok. I just wanted a friend to hang out with :(";
-			}
-			else if(reasonOut.toLowerCase().equals("nope"))
-			{
-				return "Oh ok. I just wanted a friend to hang out with :(";
-			}
-			else if(reasonOut.toLowerCase().equals("nah"))
-			{
-				return "Oh ok. I just wanted a friend to hang out with :(";
-			}
-			else if(reasonOut.toLowerCase().equals("naw"))
-			{
-				return "Oh ok. I just wanted a friend to hang out with :(";
-			}
-			else if(reasonOut.toLowerCase().equals("yes"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else if(reasonOut.toLowerCase().equals("yep"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else if(reasonOut.toLowerCase().equals("yeah"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else if(reasonOut.toLowerCase().equals("uh huh"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else if(reasonOut.toLowerCase().equals("yas"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else if(reasonOut.toLowerCase().equals("yassssssssssssssssssss"))
-			{
-				return "AWSOME! ITS GONNA BE A BLAST";
-			}
-			else
-			{
-				return "please answer my question :(";
-			}
-		}
-		else
-		{
-			return "please answer my question :(";
-		}
-	}
-	/*private String transformBecauseStatement(String statement)
-	{
+		//  Remove the final period, if there is one
 		statement = statement.trim();
 		String lastChar = statement.substring(statement
 				.length() - 1);
@@ -168,13 +215,92 @@ public class ChatBot3
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		int psn = findKeyword (statement, "because", 0);
-		String restOfStatement = statement.substring(psn + 6).trim();
-		if(statement.substring(8,16).equals("they are"))
-		return "Why";
-	}*/
+		int psn = findKeyword (statement, "because i", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "Why do you " + restOfStatement + "?";
+	}
+
+	private String transformBecauseIAm(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "because i am", 0);
+		String restOfStatement = statement.substring(psn + 12).trim();
+		return "Why are you " + restOfStatement + "?";
+	}
+
+	private String transformBecauseYou(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "because you", 0);
+		String restOfStatement = statement.substring(psn + 11).trim();
+		return "Why do I " + restOfStatement + "?";
+	}
+	private String transformBecauseYouAre(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "because you are", 0);
+		String restOfStatement = statement.substring(psn + 15).trim();
+		return "Why do I " + restOfStatement + "?";
+	}
+
+	private String transformCuzYou(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "cuz you", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		return "Why do I " + restOfStatement + "?";
+	}
+
+	private String transformCuzYouAre(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "cuz you are", 0);
+		String restOfStatement = statement.substring(psn + 11).trim();
+		return "Why do I " + restOfStatement + "?";
+	}
+
 	/**
-	 * Take a statement with "I want to <something>." and transform it into 
+	 * Take a statement with "I want to <something>." and transform it into
 	 * "Why do you want to <something>?"
 	 * @param statement the user statement, assumed to contain "I want to"
 	 * @return the transformed statement
@@ -195,9 +321,9 @@ public class ChatBot3
 		return "Why do you want to " + restOfStatement + "?";
 	}
 
-	
+
 	/**
-	 * Take a statement with "I want <something>." and transform it into 
+	 * Take a statement with "I want <something>." and transform it into
 	 * "Would you really be happy if you had <something>?"
 	 * @param statement the user statement, assumed to contain "I want"
 	 * @return the transformed statement
@@ -217,10 +343,10 @@ public class ChatBot3
 		String restOfStatement = statement.substring(psn + 6).trim();
 		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
-	
-	
+
+
 	/**
-	 * Take a statement with "I <something> you" and transform it into 
+	 * Take a statement with "I <something> you" and transform it into
 	 * "Why do you <something> me?"
 	 * @param statement the user statement, assumed to contain "I" followed by "you"
 	 * @return the transformed statement
@@ -236,17 +362,17 @@ public class ChatBot3
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		
+
 		int psnOfI = findKeyword (statement, "I", 0);
 		int psnOfYou = findKeyword (statement, "you", psnOfI);
-		
+
 		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
 		return "Why do you " + restOfStatement + " me?";
 	}
-	
 
-	
-	
+
+
+
 	/**
 	 * Search for one word in phrase. The search is not case
 	 * sensitive. This method will check that the given goal
@@ -264,7 +390,7 @@ public class ChatBot3
 	 *         statement or -1 if it's not found
 	 */
 	private int findKeyword(String statement, String goal,
-			int startPos)
+							int startPos)
 	{
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
@@ -295,9 +421,9 @@ public class ChatBot3
 			// found the word
 			if (((before.compareTo("a") < 0) || (before
 					.compareTo("z") > 0)) // before is not a
-											// letter
+					// letter
 					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0)))
+					.compareTo("z") > 0)))
 			{
 				return psn;
 			}
@@ -310,11 +436,11 @@ public class ChatBot3
 
 		return -1;
 	}
-	
+
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
 	 * This method will check that the given goal is not a substring of a longer string
-	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.
 	 * @param statement the string to search
 	 * @param goal the string to search for
 	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
@@ -323,7 +449,7 @@ public class ChatBot3
 	{
 		return findKeyword (statement, goal, 0);
 	}
-	
+
 
 
 	/**
@@ -334,25 +460,28 @@ public class ChatBot3
 	{
 		Random r = new Random ();
 		if (emotion == 0)
-		{	
+		{
 			return randomNeutralResponses [r.nextInt(randomNeutralResponses.length)];
 		}
 		if (emotion < 0)
-		{	
+		{
 			return randomAngryResponses [r.nextInt(randomAngryResponses.length)];
-		}	
+		}
 		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
 	}
-	
+
 	private String [] randomNeutralResponses = {"Interesting, tell me more",
 			"Hmmm.",
-			"Do you really think so?",
+			"Is that what most people think?",
 			"You don't say.",
-			"It's all boolean to me.",
-			"So, would you like to go for a walk?",
-			"Could you say that again?"
+			"I'm having trouble understanding. Could you elaborate?",
 	};
-	private String [] randomAngryResponses = {"Bahumbug.", "Harumph", "The rage consumes me!"};
-	private String [] randomHappyResponses = {"H A P P Y, what's that spell?", "Today is a good day", "You make me feel like a brand new pair of shoes."};
-	
+	private String [] randomAngryResponses = {"Well if wee can't have a nice discussion about football, then we cannot be friends.", "Your favorite football team sucks", "I am tired of your disrespect for the great game of football"};
+	private String [] randomHappyResponses = {"Why don't we go to a football game together? :)", "Talking with you has been a real touch down, you know what I mean?", "You know our relationship is so strong.WE SHOULD GO TO THE SUPER BOWL TOGETHER"};
+	private String getRandomQuestion()
+	{
+		Random r = new Random ();
+		return randomQuestions [r.nextInt(randomQuestions.length)];
+	}
+	private String [] randomQuestions = {"When can we meet up some other time to talk about this even more?","How are you feeling right now?","What is your favorite team?","What do you think of the giants?","How big of a football fan are you?"};
 }
